4cd327e06fe19a771139a1a4ad2dc86c92c9d7ca
Jenni <justlittleoldmejenn@gmail.com>
2021-05-26 22:32:45 -0700

    Starting on new input system player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    private float moveX;
+    private float moveY;
+    [SerializeField] private float speed = 1;
+
+    
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    private void FixedUpdate() {
+        Vector3 movement = new Vector3(moveX, 0.0f, moveY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnMove(InputValue movementValue) {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+        
+    }
+}
