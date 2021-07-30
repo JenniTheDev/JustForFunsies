4c697faa3e8188795fd19e729ac49822e4847d83
Jenni <justlittleoldmejenn@gmail.com>
2021-07-30 13:49:46 -0700

    Started Death position change

---
+++ Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour {
+
+    public void TipOverOnDeath() {
+        Debug.Log($"Fall over");
+        // rotation Z needs to go from 0 to 90
+    }
+}
